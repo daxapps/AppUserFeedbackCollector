1fb3d2780572a6bb1eb8a436c7d3585ac9a01618
Jason Crawford <daxapps777@gmail.com>
2017-12-14 21:04:40 -0600

    Enable cookies

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 
-module.exports = (app) => {
+module.exports = app => {
 	app.get(
 		"/auth/google",
 		passport.authenticate("google", {
@@ -8,5 +8,20 @@ module.exports = (app) => {
 		})
 	);
 
-	app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get(
+		"/auth/google/callback",
+		passport.authenticate("google"),
+		(err, req, res, next) => {
+			// custom error handler to catch any errors, such as TokenError
+			if (err.name === "TokenError") {
+				res.redirect("/auth/google"); // redirect them back to the login page
+			} else {
+				// Handle other errors here
+				console.log(err)
+			}
+		},
+		(req, res) => {
+			res.redirect("/surveys");
+		}
+	);
 };
