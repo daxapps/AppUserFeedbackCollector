f734b7adbf950fe33e3d7dc8f89ba0b5a92a038b
Jason Crawford <daxapps777@gmail.com>
2017-12-14 19:17:58 -0600

    Setup user model

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+
 require("./services/passport");
+require("./models/User");
 
 mongoose.connect(keys.mongoURI, {
 	useMongoClient: true
